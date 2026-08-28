70c09192ac71e567996485898b34b9eaf0a6e91e
laura0xss <laura.0xss@gmail.com>
2026-08-28 20:55:07 -0300

    enviar

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const botaoSobre = document.getElementById("sobre");
 botaoSobre.addEventListener("click", function () {
     console.log("O botão foi clicado!");
  });
+ const titulo = document.querySelector("h1");
+botaoSobre.addEventListener("click", function () {
+    titulo.textContent = "Obrigada por visitar meu portfólio!";/});
+
+Ao clicar no botão vai para o sobre
+const sobre = document.getElementById("sobre");
+
+botaoSobre.addEventListener("click", function () {
+    sobre.scrollIntoView();
+});
