6cf3db60620285b499b42cba3d983f8c0064179b
laura0xss <laura.0xss@gmail.com>
2026-08-28 20:44:56 -0300

    enviar

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//Ao clicar no botão aparece o texto no console log
+const botaoSobre = document.getElementById("botao-sobre");
+
+botaoSobre.addEventListener("click", function () {
+    console.log("O botão foi clicado!");
+ });
